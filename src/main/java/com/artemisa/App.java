e247d3ecbfc19ac7697392f552ac9c72587c19a6
Emmanuel Diéguez <emmanuel@artemisa.myfiosgateway.com>
2022-04-28 16:35:32 -0400

    Basic Spring integration example

--- src/main/java/com/artemisa/App.java
+++ src/main/java/com/artemisa/App.java
@@ -1,10 +1,29 @@
 package com.artemisa;
 
+import com.artemisa.configuration.BasicIntegrationConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.util.Scanner;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        log.info("Hello World!");
+        AbstractApplicationContext context
+                = new AnnotationConfigApplicationContext(BasicIntegrationConfig.class);
+        context.registerShutdownHook();
+
+        Scanner scanner = new Scanner(System.in);
+        log.info("Please enter q and press <enter> to exit the program: ");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if("q".equals(input.trim())) {
+                break;
+            }
+        }
+
+        System.exit(0);
     }
 }
